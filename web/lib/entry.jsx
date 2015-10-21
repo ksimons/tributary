f91510c8feb4551b1769e7e5b3816c5587a7fd16
Tor Arvid Lund <torarvid@gmail.com>
2015-10-21 14:49:34 +0200

    Refactor so Video is its own component

--- web/lib/entry.jsx
+++ web/lib/entry.jsx
@@ -1,24 +1,34 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react';
+import ReactDom from 'react-dom';
+import Video from './video.jsx';
 
-let constraints = {
-    audio: false,
-    video: {
-        mandatory: { maxWidth: 480, maxHeight: 320 }
+class Entry extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
     }
-};
 
-navigator.mediaDevices.getUserMedia(constraints)
-.then(function(stream) {
-    var vid = document.createElement("video");
-    document.getElementById('content').appendChild(vid);
-    vid.autoplay = true;
-    vid.srcObject = stream;
-})
-.catch(function(err) {
-    console.log('Ohz noez', err);
-})
+    toggleBroadcast() {
+        let newState = !this.state.broadcast;
+        this.setState({ broadcast: newState });
+    }
+
+    render() {
+        let video = (this.state.broadcast) ? <Video /> : null;
+        let broadcastText = (this.state.broadcast ? 'Stop' : 'Start') + ' broadcasting';
+        return (
+            <div>
+                <h1>Hi there!</h1>
+                <button type='button' onClick={this.toggleBroadcast.bind(this)}>
+                    {broadcastText}
+                </button>
+                {video}
+                <Video ref='video' start={this.state.broadcast} />
+            </div>
+        );
+    }
+}
 
 ReactDom.render(
-    <h1>Hi there</h1>,
+    <Entry />,
     document.getElementById('content'));
---
+++ web/lib/video.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+class Video extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        if (this.props.start) {
+            this.initVideo();
+        }
+    }
+
+    initVideo() {
+        let constraints = {
+            audio: false,
+            video: {
+                mandatory: { maxWidth: 480, maxHeight: 320 }
+            }
+        };
+
+        navigator.mediaDevices.getUserMedia(constraints)
+        .then(stream => {
+            this.setState({ stream: stream });
+        })
+        .catch(err => {
+            console.error('Ohz noez', err);
+        });
+    }
+
+    closeVideo() {
+        if (!this.state.stream) {
+            return;
+        }
+        if (typeof this.state.stream.removeTrack === 'function') { // MediaStream
+            let tracks = this.state.stream.getTracks();
+            for (var i = tracks.length - 1; i >= 0; i--) {
+                tracks[i].stop();
+                this.state.stream.removeTrack(tracks[i]);
+            };
+        } else if (typeof this.state.stream.stop === 'function') { // LocalMediaStream
+            this.state.stream.stop();
+        }
+        this.setState({ stream: null });
+    }
+
+    componentWillReceiveProps(newProps) {
+        if (newProps.start) {
+            this.initVideo();
+        } else {
+            this.closeVideo();
+        }
+    }
+
+    render() {
+        let video;
+        if (this.state.stream) {
+            let videoSrcUrl = window.URL.createObjectURL(this.state.stream);
+            video = <video autoPlay src={videoSrcUrl}></video>
+        }
+        return (
+            <div>{video}</div>
+        );
+    }
+}
+
+export default Video;
