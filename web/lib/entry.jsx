e425cf4ce4d3f902a2b166b40264fa5cd72abbb2
Tor Arvid Lund <torarvid@gmail.com>
2015-10-21 13:14:53 +0200

    Let's see some video

--- web/lib/entry.jsx
+++ web/lib/entry.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+let constraints = {
+    audio: false,
+    video: {
+        mandatory: { maxWidth: 480, maxHeight: 320 }
+    }
+};
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(stream) {
+    var vid = document.createElement("video");
+    document.getElementById('content').appendChild(vid);
+    vid.autoplay = true;
+    vid.srcObject = stream;
+})
+.catch(function(err) {
+    console.log('Ohz noez', err);
+})
+
 ReactDom.render(
-	<h1>Hi there</h1>,
-	document.getElementById('content'));
+    <h1>Hi there</h1>,
+    document.getElementById('content'));
