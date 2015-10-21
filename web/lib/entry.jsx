66eb7dce93ca7e3f563395e04206b78dfe2e1985
Tor Arvid Lund <torarvid@gmail.com>
2015-10-21 12:19:06 +0200

    JSX worked after all
    I thought it didn't for some reason... Also excluding node_modules from
    babel processing.

--- web/lib/entry.jsx
+++ web/lib/entry.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDom from "react-dom";
 
 ReactDom.render(
-	React.createElement('h1', null, 'Hi there!'),
+	<h1>Hi there</h1>,
 	document.getElementById('content'));
