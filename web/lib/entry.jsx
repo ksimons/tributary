4b7fbc14decfc3393c87d75a77ec2370daec61d7
Tor Arvid Lund <torarvid@gmail.com>
2015-10-21 12:10:54 +0200

    React included, ES6 style. Aaaall the c00lnezz

--- web/lib/entry.jsx
+++ web/lib/entry.jsx
@@ -1,1 +1,6 @@
-document.write("hello, world");
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+	React.createElement('h1', null, 'Hi there!'),
+	document.getElementById('content'));
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,9 +1,26 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
   entry: {
-    app: ["./lib/entry.js"]
+    app: ["./lib/entry.jsx"]
   },
   output: {
     path: __dirname,
     filename: "bundle.js"
   },
+  module: {
+    loaders: [
+      {
+        test: /.jsx$/,
+        loader: 'babel-loader'
+      },
+    ],
+  },
+  plugins: [
+    // Avoid publishing files when compilation failed
+    new webpack.NoErrorsPlugin(),
+  ],
+  // Create Sourcemaps for the bundle
+  devtool: 'source-map',
 };
